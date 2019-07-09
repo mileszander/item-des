4a059b74ed9141e07842c8a32a8117506b0430a4
ibrahim jabr <ibrahim.aljabr@gmail.com>
2019-07-09 09:47:16 -0500

    Setup PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,9 +8,8 @@ module.exports = {
         user: 'ubuntu',
         host: 'ec2-13-59-33-46.us-east-2.compute.amazonaws.com',
         key: '~/.ssh/jabr.pem',
-        ref: 'origin/master',
-        repo: '
-        github.com/home-de-pott/item-des.git',
+        ref: 'origin/hello-world',
+        repo: 'https://github.com/home-de-pott/item-des.git',
         path: '/home/ubuntu/server',
         'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
       }
