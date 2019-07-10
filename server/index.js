2ca5dd7a768a37fb435ea5a7fe85292b0f3b8241
ibrahim jabr <ibrahim.aljabr@gmail.com>
2019-07-10 07:20:45 -0500

    trying to serve my site

--- server/index.js
+++ server/index.js
@@ -6,10 +6,8 @@ const {PORT,HOST} = require('../config.js')
 const port = PORT || 3022;
 const host = HOST || '0.0.0.0'
  app.use(cors())
- app.use(express.static('public'))
+ app.use(express.static('client'))
  
-app.get('/', function (req, res) {
-  res.send('in the server')
-})
+
  
 app.listen(port,host,()=>console.log('listning on host: '+host+' port: '+port))
