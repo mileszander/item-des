7f97da7b960132af8c59f6c971de7aab1b6986d5
ibrahim jabr <ibrahim.aljabr@gmail.com>
2019-07-10 08:41:41 -0500

    trying

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const path = require('path')
-const cors = require('cors')
-const app = express()
-const {PORT,HOST} = require('../config.js')
-const port = PORT || 3000;
-const host = HOST || '0.0.0.0'
- app.use(cors())
- app.use(express.static('client'))
+// const express = require('express')
+// const path = require('path')
+// const cors = require('cors')
+// const app = express()
+// const {PORT,HOST} = require('../config.js')
+// const port = PORT || 3000;
+// const host = HOST || '0.0.0.0'
+//  app.use(cors())
+//  app.use(express.static('client'))
  
 
  
-app.listen(3000,()=>console.log('listning on host: '+host+' port: '+port))
+// app.listen(3000,()=>console.log('listning on host: '+host+' port: '+port))
