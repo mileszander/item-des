4c35c49bbdccd0af167209615af2cb407859dacd
ibrahim jabr <ibrahim.aljabr@gmail.com>
2019-07-17 11:21:45 -0500

    trying to debloid

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,11 +6,11 @@ module.exports = {
     deploy: {
       production: {
         user: 'ubuntu',
-        host: 'ec2-13-59-33-46.us-east-2.compute.amazonaws.com',
-        key: '~/.ssh/jabr.pem',
+        host: 'ec2-3-18-113-157.us-east-2.compute.amazonaws.com',
+        key: '~/.ssh/mvropdoeafposafapso.pem',
         ref: 'origin/hello-world',
         repo: 'https://github.com/home-de-pott/item-des.git',
-        path: '/home/ubuntu/server',
+        path: '/home/ubuntu',
         'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
       }
     }
